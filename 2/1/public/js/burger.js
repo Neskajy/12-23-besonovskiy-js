847384063b439d53aed86286676b0b6734b8a840
unknown <besonnovskiyyy@gmail.com>
2024-10-19 23:42:32 +0300

    Добавить функцию удаления профиля через fetch

--- 2/1/public/js/burger.js
+++ 2/1/public/js/burger.js
@@ -1,6 +1,6 @@
 const btn__modal = document.querySelector('.button.burger-menu');
 const header = document.querySelector('.header');
-const burger__modal = document.querySelector('.modal__burger');
+const burger__modal = document.querySelector('.menu__burger');
 
 burger__modal.style.height = `${window.innerHeight - parseFloat(getComputedStyle(header).height)}px`;
 burger__modal.style.top = `-${parseFloat(getComputedStyle(header).marginBottom)}px`
@@ -8,7 +8,6 @@ burger__modal.style.top = `-${parseFloat(getComputedStyle(header).marginBottom)}
 btn__modal.addEventListener('click', () => {
     btn__modal.classList.toggle('active');
     burger__modal.classList.toggle('active');
-    // document.body.style.overflow = 'hidden';
 });
 
 console.log(parseFloat(getComputedStyle(header).marginBottom));
