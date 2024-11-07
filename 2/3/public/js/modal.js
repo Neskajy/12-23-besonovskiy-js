ce6751090efcc61647de410008f4538289156049
Бесоновский Николай Александрович <352304@mck-ktits.ru>
2024-11-07 10:50:11 +0300

    update 3/3

--- 2/3/public/js/modal.js
+++ 2/3/public/js/modal.js
@@ -1,55 +1,26 @@
-const openModal = document.querySelector('button.delete__profile');
 const closeModal = document.querySelector('.close__modal');
-const cancelDelete = document.querySelector('.cancel');
-const deleteProfile = document.querySelector('.delete');
-const myModal = document.querySelector('.my-modal');
-const modalWhenActive = document.querySelector('.modalWhenActive');
+const button = document.querySelector('.cancel');
+const myModal = document.querySelector('.annoying__modal');
 
-const durovProfile = {
-    userId: "5",
-    token: "e#J$#IFE3u423ef"
-};
-
-
-openModal.addEventListener('click', () => {
-    myModal.classList.toggle('active');
-    modalWhenActive.classList.toggle('active');
-});
 closeModal.addEventListener('click', () => {
     myModal.classList.toggle('active');
-    modalWhenActive.classList.toggle('active');
-});
-cancelDelete.addEventListener('click', () => {
-    myModal.classList.toggle('active');
-    modalWhenActive.classList.toggle('active');
-});
-modalWhenActive.addEventListener('click', () => {
-    myModal.classList.toggle('active');
-    modalWhenActive.classList.toggle('active');
-});
-deleteProfile.addEventListener('click', () => {
-    myModal.classList.toggle('active');
-    modalWhenActive.classList.toggle('active');
-    fdeleteProfile(event, durovProfile.userId, durovProfile.token);
 });
 
-
-async function fdeleteProfile(event, userId, token) {
-    event.preventDefault();
-
-    console.log(userId)
-
-    try {
-        let response = fetch(`https://fakestoreapi.com/products/${userId}`, {
-            method: "DELETE"
-        });
-        // const deletedUsed = await response.json();
-
-        alert("Your profile succesfull deleted!");
-
-        location.reload();
-    } catch {
-
+let increment = 10;
+let time = 10;
+function changeCounterText() {
+    button.innerHTML += `<span class="counter">${increment}</span>`;
+    if (button.querySelector('class')) {
+        button.querySelector('class').remove();
     }
+    increment++;
 }
 
+let timer = setInterval(() => {
+    if (time > 0) {
+        changeCounterText();
+    } else {
+        clearInterval(item);
+        button.querySelector('class') = '';
+    }
+}, 1000);
