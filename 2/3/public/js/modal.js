fa812ac542fe87227fb2ef0b0d174af0767717a5
unknown <besonovskiyyy@gmail.com>
2024-11-14 11:10:26 +0300

    update 3/3

--- 2/3/public/js/modal.js
+++ 2/3/public/js/modal.js
@@ -1,26 +1,53 @@
 const closeModal = document.querySelector('.close__modal');
 const button = document.querySelector('.cancel');
 const myModal = document.querySelector('.annoying__modal');
+const accordionsModal = document.querySelector('.FAQS');
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+}
+
+
+function openModal() {
+    myModal.classList.toggle('active');
+}
 
 closeModal.addEventListener('click', () => {
     myModal.classList.toggle('active');
 });
 
-let increment = 10;
+let observer;
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            openModal();
+        }
+        
+        prevRatio = entry.intersectRatio;
+    });
+}
+
+observer = new IntersectionObserver(handleIntersect, options);
+
+observer.observe(accordionsModal);
 let time = 10;
 function changeCounterText() {
-    button.innerHTML += `<span class="counter">${increment}</span>`;
-    if (button.querySelector('class')) {
-        button.querySelector('class').remove();
-    }
-    increment++;
+    for (children of Array.from(button.children)) {
+        if (child.className === 'counter') {
+            button.innerHTML = `<span class="counter">${time}</span>`;
+            time--;
+            return 0;
+        }
+    };
+    button.innerHTML += `<span class="counter">${time}</span>`;
+    time--;
 }
 
 let timer = setInterval(() => {
     if (time > 0) {
         changeCounterText();
-    } else {
-        clearInterval(item);
-        button.querySelector('class') = '';
     }
 }, 1000);
